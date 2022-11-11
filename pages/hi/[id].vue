edc92e7de0773d2cff52a11a93e86aa58135d603
KienKhang <trankienkhang2210@gmail.com>
2022-11-11 15:20:06 +0700

    feat: add ticket 1

--- pages/hi/[id].vue
+++ pages/hi/[id].vue
@@ -18,8 +18,15 @@ const title ='Những Thành Phố Mơ Màng 2022'
 const description = 'Những Thành Phố Mơ Màng - Một show âm nhạc không còn quá xa lạ với các bạn trẻ yêu thích dòng nhạc Indie và Underground ở Việt Nam.'
 const imageLink = 'https://images.tkbcdn.com/1/1560/600/Upload/eventcover/2022/10/31/70C7B5.jpg'
 
-const handleGotoTicket = ()=>{
+
+const t =  'LIVESHOW THE BEST OF NHƯ QUỲNH | 12.11.2022 | NHÀ HÁT HOÀ BÌNH'
+const d = 'LIVESHOW THE BEST OF NHƯ QUỲNH là cột mốc đánh dấu 30 năm ca khúc người tình mùa đông'
+const i = 'https://images.tkbcdn.com/1/1560/600/Upload/eventcover/2022/10/25/829FD9.jpg'
+const handleGotoTicket2 = ()=>{
   router.push({path:'/meta/2',query:{t:title,d:description,i:imageLink}})
+}
+const handleGotoTicket1 = ()=>{
+  router.push({path:'/meta/1',query:{t,d,i}})
 }
 </script>
 
@@ -32,7 +39,8 @@ const handleGotoTicket = ()=>{
     <div text-xl>
       {{ name }}!
     </div>
-    <button @click="handleGotoTicket">Go to ticket</button>
+    <button @click="handleGotoTicket2">Go to ticket 2</button>
+    <button @click="handleGotoTicket1">Go to ticket 1</button>
     <template v-if="user.otherNames.length">
       <p text-sm my-4>
         <span op-50>Also as known as:</span>
